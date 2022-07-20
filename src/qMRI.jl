bf7d727f4b5293d784e8b2ecd87acad2ee3bf481
aTrotier <a.trotier@gmail.com>
2022-07-20 11:46:06 +0200

    add fitT2 and test

--- src/qMRI.jl
+++ src/qMRI.jl
@@ -1,5 +1,47 @@
 module qMRI
 
-# Write your package code here.
+export T2ExponentialFit
+using LsqFit
+
+"""
+    T2ExponentialFit(ima::Array{T,4},t=Any[],p0=nothing) where T
+
+Fit the relaxation parameters T2 with the equation : ``S(t) = M_0 \\exp(-\\frac{t}{T2}) ``.
+
+# Arguments
+- `ima::Array{T,4}`: image with dimension (x,y,z,t)
+- `t=Real[]`: times vector in ms
+- `p0=nothing`: starting values for fit, if empty p0=[maximum(ima),30]
+
+# Keywords
+- `removePoint::Bool=true`: remove the first point before fitting
+
+# Returns maps with format (x,y,z)
+- M₀ maps (no unit)
+- T₂ maps (ms)
+
+"""
+function T2ExponentialFit(ima::Array{T,4},t=Union(Real[],StepRange),p0=nothing;removePoint::Bool=true) where T
+    @assert size(ima,4) == length(t)
+
+    if isnothing(p0); p0=[maximum(ima),30]; end
+
+
+    model(t, p) = p[1] * exp.(-t / p[2])
+    
+    #fit_vec = LsqFitResult[]
+    M0 = zeros(Float64,size(ima)[1:3])
+    T2 = zeros(Float64,size(ima)[1:3])
+
+    ima = reshape(ima,:,size(ima,4))
+
+    for i = 1:size(ima,1)
+        fit=curve_fit(model, t, ima[i,:], p0)
+        M0[i] = fit.param[1]
+        T2[i] = fit.param[2]
+    end
+
+    return M0,T2
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,14 @@ using qMRI
 using Test
 
 @testset "qMRI.jl" begin
-    # Write your tests here.
+    t = 1:1:100
+    T2 = 10 # ms
+    M0 = 1
+
+    S = M0 * exp.(-t/T2)
+    S = reshape(S,1,1,1,:)
+
+    M0_fit,T2_fit = T2ExponentialFit(S,t)
+
+    @test abs.(M0 - M0_fit[1]) < 10e-6 && abs.(T2 - T2_fit[1]) < 10e-6
 end
