cea52af6969c81c10ab8f754c9173944f3058f67
Documenter.jl <documenter@juliadocs.github.io>
2022-07-20 09:02:02 +0000

    build based on 15a9767

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = qMRI","category":"page"},{"location":"#qMRI","page":"Home","title":"qMRI","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for qMRI.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [qMRI]","category":"page"}]
+}
