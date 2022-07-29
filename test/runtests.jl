83d5b618e261b635949ed328d1ffcc9e0d3f89b5
aTrotier <a.trotier@gmail.com>
2022-07-29 11:30:20 +0200

    refactor functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,27 +5,27 @@ using Random
 
 @testset "qMRI.jl" begin
     Random.seed!(42)
-    @info "test T2ExpFit"
+    @info "T2Fit"
     t = 1:1:100
     T2 = 10 # ms
     M0 = 1
 
     S = M0 * exp.(-t/T2)
     S = reshape(S,1,1,1,:)
-    M0_fit,T2_fit = T2ExpFit(S,[t...])
+    M0_fit,T2_fit = T2Fit_Exp(S,[t...])
     @test abs.(M0 - M0_fit[1]) < 10e-6 && abs.(T2 - T2_fit[1]) < 10e-6
 
-    @info "test T2ExpFit + remove"
+    @info "T2Fit + remove"
     t = 1:1:100
     T2 = 10 # ms
     M0 = 1
 
     S = M0 * exp.(-t/T2)
     S = reshape(S,1,1,1,:)
-    M0_fit,T2_fit = T2ExpFit(S,[t...],removePoint=true)
+    M0_fit,T2_fit = T2Fit_Exp(S,[t...],removePoint=true)
     @test abs.(M0 - M0_fit[1]) < 10e-6 && abs.(T2 - T2_fit[1]) < 10e-6
 
-    @info "test T2NoiseExpFit"
+    @info "T2Fit_ExpNoise"
     t = 1:1:100
     T2 = 30 # ms
     M0 = 1000
@@ -34,11 +34,11 @@ using Random
 
     S = sqrt.((M0 * exp.(-t/T2)).^2 .+ 2*L*σ^2)
     S = reshape(S,1,1,1,:)
-    M0_fit,T2_fit,σ_fit = T2NoiseExpFit(S,t;L=L)
+    M0_fit,T2_fit,σ_fit = T2Fit_ExpNoise(S,t;L=L)
     @test abs.(M0 - M0_fit[1]) < 10e-6 && abs.(T2 - T2_fit[1]) < 10e-6 && abs.(σ - σ_fit[1]) < 10e-6
 
     # epg fit
-    @info "fit epg"
+    @info "T2Fit_EpgNoise"
     # static parameter
     params = Dict{Symbol,Any}()
     params[:T1] = 1000.0
@@ -56,12 +56,10 @@ using Random
     echos_noise = reshape(echos_noise,1,1,1,:)
     # fit and check results
     x0 = [1,70.0,0.9,0]
-    M0_maps, T2_maps, delta_maps, noise_maps = T2EpgNoiseFit(echos_noise,params,x0)
+    M0_maps, T2_maps, delta_maps, noise_maps = T2Fit_EpgNoise(echos_noise,params,x0)
 
     @test (abs(M0_maps[1]) - M0)/M0 < 0.05
     @test (abs(T2_maps[1]) - params[:T2])/params[:T2] < 0.05
     @test (abs(delta_maps[1]) - params[:delta])/params[:delta] < 0.05
     @test (abs(noise_maps[1])/sqrt(2) - σ)/σ < 0.05
 end
-
-
